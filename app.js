c53db4c4b73437a5cd1a3a52392e7a23c7318177
Hidenori Oka <vwkr6t@gmail.com>
2021-02-17 00:54:03 +0900

    [add] get Types

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ const app = express();
 
 const PORT_NUMBER = 8080;
 
+// ルーティング設定
 app.get('/', (req, res) => {
-  res.send('<h1>ROOT</h1>');
+  // HTMLファイルを返却
+  res.sendFile(`${__dirname}/index.html`);
 })
 
-app.get('/test', (req, res) => {
-  res.send('<h1>TEST</h1>');
-});
+app.get('/getJson', (req, res) => {
+  const data = {
+    'message': 'hello',
+    'date': '2021-02-17',
+  }
+
+  res.json(data);
+})
 
 app.listen(PORT_NUMBER, () => {
   console.log('Server listen on ' + PORT_NUMBER);
